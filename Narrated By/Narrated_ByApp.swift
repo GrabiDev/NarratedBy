49aef8ec8fc59430ad0c17924ff2f11920cfa668
Mateusz Grabara <mateusz@grabara.eu>
2022-10-15 23:01:27 +0100

    Initial Commit

---
+++ Narrated By/Narrated_ByApp.swift
@@ -0,0 +1,17 @@
+//
+//  Narrated_ByApp.swift
+//  Narrated By
+//
+//  Created by Mateusz Grabara on 15/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Narrated_ByApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
